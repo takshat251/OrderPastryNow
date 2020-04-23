404df56cc8db4f720b08f18f1ca707164c1b4458
takshat251 <takshat251@gmail.com>
2020-04-23 20:31:57 +0530

    OrderPizzaNow commit 1

---
+++ app/src/main/java/com/example/admin/order_pastry_now/MenuItems.java
@@ -0,0 +1,13 @@
+package com.example.admin.order_pastry_now;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_items);
+    }
+}
